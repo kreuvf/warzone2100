kreuvf/warzone2100

commit bfa9c66d52285cf337d2ca7cdb634bcbbe8a62f5

* Repair VTOLs standing on a rearm pad without additional player interaction

src/structure.cpp
@@ -3066,7 +3066,21 @@ static void aiUpdateStructure(STRUCTURE *psStructure, bool isMission)
 					{
 						psChosenObj = psDroid;
 					}
+					if (isVtolDroid(psDroid))
+					{
+						// for VTOLs just sitting on rearm pads, we repair them if they are damaged
+						// Check if the units is on the pad or not (within coordinates of center ± 64)
+						// Note: This is NOT the same as 'distance to center is <= 64'!
+						xdiff = abs((SDWORD)psDroid->pos.x - (SDWORD)psStructure->pos.x);
+						ydiff = abs((SDWORD)psDroid->pos.y - (SDWORD)psStructure->pos.y);
+						if (xdiff <= 64 && ydiff <= 64 && psDroid->body < psDroid->originalBody)
+						{
+							psChosenObj = psDroid;
+						}
+					}
 				}
+			
+				
 				if (!psChosenObj) // None available? Try allies.
 				{
 					for (i=0; i<MAX_PLAYERS; i++)

commit a266dfad70ea41242feac3d7e26b13235d922a34

* Fix bug introduced in bfa9c66d52285cf337d2ca7cdb634bcbbe8a62f5 	* Damaged VTOLs above a rearm pad got their orders changed to "repair here" 	* Additional check for z coordinate fixes this behaviour

src/structure.cpp
@@ -3074,7 +3074,7 @@ static void aiUpdateStructure(STRUCTURE *psStructure, bool isMission)
 						// Note: This is NOT the same as 'distance to center is <= 64'!
 						xdiff = abs((SDWORD)psDroid->pos.x - (SDWORD)psStructure->pos.x);
 						ydiff = abs((SDWORD)psDroid->pos.y - (SDWORD)psStructure->pos.y);
-						if (xdiff <= 64 && ydiff <= 64 && psDroid->body < psDroid->originalBody)
+						if (xdiff <= 64 && ydiff <= 64 && psDroid->pos.z == psStructure->pos.z && psDroid->body < psDroid->originalBody)
 						{
 							psChosenObj = psDroid;
 						}